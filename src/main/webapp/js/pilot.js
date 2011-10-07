fe05362bb1f98139f508c9ad76588bc7d9b948b1
Clifton Snyder <cliff.snyder@gmail.com>
2011-10-07 00:44:25 -0400

    * reworking the directory hierarchy a bit

--- src/main/webapp/js/pilot.js
+++ src/main/webapp/js/pilot.js
@@ -1,4 +1,28 @@
 /* pilot.js */
-function pilot_init(){
+function pilot_init() {
 	$('#test').clock();
+
+	$.ajax({
+		url : 'http://192.168.0.123:8080/pilot/rest/list',
+		dataType : 'json',
+		success : function(data) {
+			$('#test2').append(data);
+//			var items = [];
+//
+//			$.each(list, function(data) {
+//				$.each(data, function(key, val) {
+//					items.push('<li id="' + key + '">' + val + '</li>');
+//				});
+//			});
+//
+//			$('<ul/>', {
+//				'class' : 'my-new-list',
+//				html : items.join('')
+//			}).appendTo('#test2');
+			
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			$('#test2').append("error retrieving data: " + textStatus);
+		}
+	});
 }
